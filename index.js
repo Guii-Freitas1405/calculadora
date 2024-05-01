64c64c2f24ff24c07f92781763f440345923b782
Guilherme Freitas <guiseixasfreitas@gmail.com>
2024-05-01 19:29:59 -0300

    subtração

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ console.log(input2);
 const button = document.getElementById("button");
 console.log(button);
 
+const button2 = document.getElementById("button2");
+console.log(button2);
+
 const paragrafo = document.getElementById("result");
 console.log(paragrafo);
 
@@ -18,4 +21,13 @@ function somar() {
   paragrafo.innerText = res;
 }
 
+function diminuir() {
+  const valor1 = Number(input1.value);
+  const valor2 = Number(input2.value);
+
+  const res = valor1 - valor2;
+  paragrafo.innerText = res;
+}
+
 button.addEventListener("click", somar);
+button2.addEventListener("click", diminuir);
