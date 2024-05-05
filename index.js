0637d29cb16f07b0baeb04d05ff4d783ab2fe696
Guilherme Freitas <guiseixasfreitas@gmail.com>
2024-05-05 17:40:49 -0300

    nova calculadora

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
-const input1 = document.getElementById("inp1");
-console.log(input1);
+// const numbers = [0, 1, 2, 3, 4, 5];
 
-const input2 = document.getElementById("inp2");
-console.log(input2);
+// function onEach(number) {
+//   console.log(number);
+// }
 
-const button = document.getElementById("button");
-console.log(button);
+// numbers.forEach(onEach);
 
-const button2 = document.getElementById("button2");
-console.log(button2);
+// const buttons = document.querySelectorAll("button");
 
-const paragrafo = document.getElementById("result");
-console.log(paragrafo);
+// function showHello() {
+//   console.log("Olá");
+// }
 
-function somar() {
-  const valor1 = Number(input1.value);
-  const valor2 = Number(input2.value);
+// function qualquerNome(button) {
+//   button.addEventListener("click", showHello);
+// }
 
-  const res = valor1 + valor2;
-  paragrafo.innerText = res;
-}
-
-function diminuir() {
-  const valor1 = Number(input1.value);
-  const valor2 = Number(input2.value);
-
-  const res = valor1 - valor2;
-  paragrafo.innerText = res;
-}
-
-button.addEventListener("click", somar);
-button2.addEventListener("click", diminuir);
+// buttons.forEach(qualquerNome);
